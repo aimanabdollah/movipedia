593c0e2fb41e29f818d95c3ff8213d1e320c58f6
Muhammad Aiman <aimanabdollah@gmail.com>
2023-08-29 11:36:42 +0800

    Updated project files

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,24 +1,83 @@
+import React, { useState } from "react";
 import Movie from "./Movie";
 
 const MainContent = ({ movies, year, genreName, handleLoadMoreClick }) => {
+  const [searchQuery, setSearchQuery] = useState(""); // State for search query
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const renderSearchForm = (
+    <div className="col-md-6 offset-md-3">
+      <form>
+        <div className="input-group py-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search movies..."
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+          <button className="btn btn-outline-primary" type="button">
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+
+  const renderNoSearchResults = (
+    <div className="col text-center">
+      <img
+        src="/no-found-img.png"
+        alt="No search found"
+        className="img-fluid"
+        style={{ maxWidth: "300px" }}
+      />
+      <h5 className="text-white">Oops! No search results found.</h5>
+    </div>
+  );
+
+  const renderMovies = (
+    <div className="row">
+      {filteredMovies.map((movie) => (
+        <Movie key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
+
+  const renderLoadMoreButton = filteredMovies.length > 12 && (
+    <div className="row">
+      <div className="col text-center">
+        <button className="btn btn-dark" onClick={handleLoadMoreClick}>
+          Load More...
+        </button>
+      </div>
+    </div>
+  );
+
   return (
     <main className="pb-5">
       <div className="container">
-        <h2 className="py-5 text-white text-center">
+        <h2 className="py-3 text-white text-center">
           {`Best Movie ${year}, Genre: ${genreName}`}
         </h2>
+
+        {/* Search Form */}
+        <div className="row mb-3">{renderSearchForm}</div>
+
+        {/* Display Filtered Movies or "No search found" */}
         <div className="row">
-          {movies.map((movie) => (
-            <Movie key={movie.id} movie={movie} />
-          ))}
+          {filteredMovies.length === 0 ? renderNoSearchResults : renderMovies}
         </div>
-        <div className="row">
-          <div className="col text-center">
-            <button className="btn btn-dark" onClick={handleLoadMoreClick}>
-              Load More...
-            </button>
-          </div>
-        </div>
+
+        {/* "Load More..." Button */}
+        {filteredMovies.length > 0 && renderLoadMoreButton}
       </div>
     </main>
   );
